c8ddcc2ca963971810dd681e5a3236241615e816
durkasrimani <durkasrimani937@gmail.com>
2026-06-24 19:59:28 +0530

    floor

---
+++ floor.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class floor{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        int ans=floor(arr,target);
+        System.out.print(ans);
+    }
+    public static int floor(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target>arr[mid]){
+                start=mid+1;
+            }
+            else if(target<arr[mid]){
+                end=end-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return arr[end];
+    }
+}
